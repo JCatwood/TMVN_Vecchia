2379154e0fd5d585dccb3ce6dcffb468840f0b07
Jian Cao <jian.cao@kaust.edu.sa>
2023-02-28 16:28:45 -0600

    Added the dense A matrix as part of return

--- funcs/vecc_cond_mean_var.R
+++ funcs/vecc_cond_mean_var.R
@@ -7,6 +7,7 @@ vecc_cond_mean_var <- function(covMat, NNarray){
   cond_mean_coeff <- matrix(0, n, m)
   cond_var <- rep(NA, n)
   cond_var[1] <- covMat[1, 1]
+  A <- matrix(0, n, n)
   for(i in 2 : n){
     ind_cond <- NNarray[2 : min(i, (m + 1))]
     cov_mat_sub_inv <- solve(covMat[ind_cond, ind_cond])
@@ -14,7 +15,8 @@ vecc_cond_mean_var <- function(covMat, NNarray){
     cond_var[i] <- covMat[i, i] - 
       t(cov_vec_sub) %*% cov_mat_sub_inv %*% cov_vec_sub
     cond_mean_coeff[i, min(i - 1, m)] <- t(cov_vec_sub) %*% cov_mat_sub_inv
+    A[i, ind_cond] <- cond_mean_coeff[i, min(i - 1, m)]
   }
   return(list(cond_mean_coeff = cond_mean_coeff, cond_var = cond_var, 
-              nn = NNarray))
+              nn = NNarray, A = A))
 }
