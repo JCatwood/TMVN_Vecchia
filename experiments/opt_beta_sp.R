a37878399d7ee7973bffc8ea8072d3fc9e9c1e5a
Jian Cao <jian.cao@kaust.edu.sa>
2023-04-12 13:08:02 -0500

    Added comparison with optim

--- experiments/opt_beta_sp.R
+++ experiments/opt_beta_sp.R
@@ -38,48 +38,81 @@ for (i in 1:length(a_list_ord)) {
   b_ord <- b_list_ord[[i]]
   trunc_expect <- etruncnorm(a_ord, b_ord)
   x0 <- c(trunc_expect[-n], rep(0, n - 1))
-  solv_idea_5 <- nleqslv(x0,
-    fn = grad_idea5,
-    jac = jac_idea5,
-    veccCondMeanVarObj = vecc_cond_mean_var_obj,
-    a = a_ord, b = b_ord,
-    global = "pwldog",
-    method = "Newton",
-    control = list(maxit = 500L)
-  )
-  x0 <- c(trunc_expect, rep(0, n))
-  solv_idea_5_sp <- my_nleqslv(
-    x0,
-    fn = function(x, ...) {
-      ret <- grad_jacprod_jacsolv_idea5(x, ...,
-        retJac = F,
-        retProd = F, retSolv = F
-      )
-      ret$grad
-    },
-    jacTransFn = function(x, ...) {
-      ret <- grad_jacprod_jacsolv_idea5(x, ...,
-        retJac = F,
-        retProd = T, retSolv = F
-      )
-      ret$jac_grad
-    },
-    jacInvFn = function(x, ...) {
-      ret <- grad_jacprod_jacsolv_idea5(x, ...,
-        retJac = F,
-        retProd = F, retSolv = T
-      )
-      ret$jac_inv_grad
-    },
-    veccCondMeanVarObj = vecc_cond_mean_var_obj_sp,
-    a = a_ord, b = b_ord, verbose = F,
-    control = list(maxit = 1000L)
-  )
+  x0_all <- c(trunc_expect, rep(0, n))
+  cat("Dense nleqslv used", system.time(
+    solv_idea_5 <- nleqslv(x0,
+      fn = grad_idea5,
+      jac = jac_idea5,
+      veccCondMeanVarObj = vecc_cond_mean_var_obj,
+      a = a_ord, b = b_ord,
+      global = "pwldog",
+      method = "Newton",
+      control = list(maxit = 500L)
+    )
+  )[[3]], "seconds\n")
+  cat("Sparse nleqslv used", system.time(
+    solv_idea_5_sp <- my_nleqslv(
+      x0_all,
+      fn = function(x, ...) {
+        ret <- grad_jacprod_jacsolv_idea5(x, ...,
+          retJac = F,
+          retProd = F, retSolv = F
+        )
+        ret$grad
+      },
+      jacTransFn = function(x, ...) {
+        ret <- grad_jacprod_jacsolv_idea5(x, ...,
+          retJac = F,
+          retProd = T, retSolv = F
+        )
+        ret$jac_grad
+      },
+      jacInvFn = function(x, ...) {
+        ret <- grad_jacprod_jacsolv_idea5(x, ...,
+          retJac = F,
+          retProd = F, retSolv = T
+        )
+        ret$jac_inv_grad
+      },
+      veccCondMeanVarObj = vecc_cond_mean_var_obj_sp,
+      a = a_ord, b = b_ord, verbose = F,
+      control = list(maxit = 1000L)
+    )
+  )[[3]], "seconds\n")
+  cat("Sparse optim used", system.time(
+    solv_idea_5_sp_optim <- optim(
+      x0_all,
+      fn = function(x, ...) {
+        ret <- grad_jacprod_jacsolv_idea5(x, ...,
+          retJac = F,
+          retProd = F, retSolv = F
+        )
+        0.5 * sum((ret$grad)^2)
+      },
+      gr = function(x, ...) {
+        ret <- grad_jacprod_jacsolv_idea5(x, ...,
+          retJac = F,
+          retProd = T, retSolv = F
+        )
+        ret$jac_grad
+      },
+      method = "L-BFGS-B",
+      veccCondMeanVarObj = vecc_cond_mean_var_obj_sp,
+      a = a_ord, b = b_ord, verbose = F,
+      control = list(maxit = 1000L)
+    )
+  )[[3]], "seconds\n")
+
+
   ### Check solution consistency -------------------------------
   cat("Terminal codes of dense optimization is", solv_idea_5$termcd, "\n")
   cat("Terminal codes of sparse optimization is", solv_idea_5_sp$code, "\n")
   cat(
-    "Difference dense optimization and sparse optimization is",
+    "Difference dense nleqslv and sparse nleqslv is",
     sum(abs(solv_idea_5$x - solv_idea_5_sp$x[-c(n, 2 * n)])), "\n"
+  )
+  cat(
+    "Difference dense nleqslv and sparse optim is",
+    sum(abs(solv_idea_5$x - solv_idea_5_sp_optim$par[-c(n, 2 * n)])), "\n"
   )
 }
