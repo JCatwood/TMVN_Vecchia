078d9cef140499a4f35518f74da04b5733283c6e
Jian Cao <jian.cao@kaust.edu.sa>
2023-04-18 13:37:56 -0500

    Reorder using covariance matrix

--- TMVN_Vecchia_Rpkg/R/FIC_univar_reorder.R
+++ TMVN_Vecchia_Rpkg/R/FIC_univar_reorder.R
@@ -14,25 +14,47 @@ library(truncnorm)
 #' a <- rep(-Inf, n)
 #' b <- seq(from = -3, to = 3, length.out = n)
 #' cat("The output order should be roughly increasing after", m, "numbers \n")
-#' cat(FIC_univar_reorder(a, b, locs, cov_name, covparms, m))
-FIC_univar_reorder <- function(a, b, locs, covName, covParms, m) {
-  n <- nrow(locs)
+#' cat(FIC_univar_reorder(a, b, m, locs, cov_name, covparms))
+FIC_univar_reorder <- function(a, b, m, locs = NULL, covName = NULL,
+                               covParms = NULL, covMat = NULL) {
+  if (!is.null(covMat)) {
+    n <- nrow(covMat)
+  } else {
+    n <- nrow(locs)
+  }
   m <- min(m, n - 1)
   ## standardize ---------------------
-  sd_par <- sqrt(covParms[1])
-  a <- a / sd_par
-  b <- b / sd_par
-  covParms[1] <- 1
+  if (!is.null(covMat)) {
+    sd_par <- sqrt(diag(covMat))
+    a <- a / sd_par
+    b <- b / sd_par
+    covMat <- t(t(covMat / sd_par) / sd_par)
+  } else {
+    sd_par <- sqrt(covParms[1])
+    a <- a / sd_par
+    b <- b / sd_par
+    covParms[1] <- 1
+  }
   ## maxmin order --------------------------
-  odr_maxmin <- GpGp::order_maxmin(locs)
+  if (!is.null(covMat)) {
+    odr_maxmin <- sample(1:n, n, F)
+  } else {
+    odr_maxmin <- GpGp::order_maxmin(locs)
+  }
   odr_FIC_univar <- odr_maxmin
   a <- a[odr_maxmin]
   b <- b[odr_maxmin]
   locs <- locs[odr_maxmin, , drop = F]
   ## cov func -------------------------------
-  cov_func_GpGp <- getFromNamespace(covName, "GpGp")
-  cov_func <- function(ind) {
-    cov_func_GpGp(covParms, locs[ind, , drop = F])
+  if (!is.null(covMat)) {
+    cov_func <- function(ind) {
+      covMat[ind, ind, drop = F]
+    }
+  } else {
+    cov_func_GpGp <- getFromNamespace(covName, "GpGp")
+    cov_func <- function(ind) {
+      cov_func_GpGp(covParms, locs[ind, , drop = F])
+    }
   }
   ## TMVN expectation ------------------------------
   x_first_m <- truncnorm::etruncnorm(a[1:m], b[1:m])
