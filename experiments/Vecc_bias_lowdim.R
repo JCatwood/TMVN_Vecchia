9edf5d306b363294f819f77a6e49aa1770e6ab9b
Jian Cao <jian.cao@kaust.edu.sa>
2023-06-16 13:05:33 -0500

    Added low-dim problem 2 and removed the univar reorder from TruncatedNormal

--- experiments/Vecc_bias_lowdim.R
+++ experiments/Vecc_bias_lowdim.R
@@ -16,10 +16,22 @@ prob1_gen <- function(n, d, retDenseCov = F) {
   cov_parms <- c(1.0, 0.1, 0.01)
   cov_name <- "matern15_isotropic"
   cov_mat <- get(cov_name)(cov_parms, locs)
-  odr <- TruncatedNormal::cholperm(cov_mat, a, b)$perm
-  a <- a[odr]
-  b <- b[odr]
-  locs <- locs[odr, , drop = F]
+  # odr <- TruncatedNormal::cholperm(cov_mat, a, b)$perm
+  # a <- a[odr]
+  # b <- b[odr]
+  # locs <- locs[odr, , drop = F]
+  # cov_mat <- get(cov_name)(cov_parms, locs)
+  return(list(
+    a = a, b = b, locs = locs, cov_parms = cov_parms,
+    cov_name = cov_name, cov_mat = cov_mat
+  ))
+}
+prob2_gen <- function(n, d, retDenseCov = F) {
+  locs <- latin_gen(n, d)
+  a <- rep(-Inf, n)
+  b <- -runif(n, 0, 2)
+  cov_parms <- c(1.0, 0.1, 0.01)
+  cov_name <- "matern15_isotropic"
   cov_mat <- get(cov_name)(cov_parms, locs)
   return(list(
     a = a, b = b, locs = locs, cov_parms = cov_parms,
@@ -31,7 +43,8 @@ set.seed(123)
 n <- 900
 d <- 2
 m_vec <- seq(from = 10, to = 30, by = 10)
-prob_obj <- prob1_gen(n, d, retDenseCov = T)
+prob_ind <- 1
+prob_obj <- get(paste0("prob", prob_ind, "_gen"))(n, d, retDenseCov = T)
 a <- prob_obj$a
 b <- prob_obj$b
 locs <- prob_obj$locs
@@ -50,7 +63,7 @@ for (i in 1:niter) {
     m <- m_vec[j]
     time_Vecc[j] <- system.time(est_Vecc[j] <- VeccTMVN::pmvn(a, b, 0,
       locs = locs, covName = cov_name,
-      reorder = 0, covParms = cov_parms,
+      reorder = 1, covParms = cov_parms,
       m = m, verbose = T,
       NLevel1 = 10, NLevel2 = 1e3,
     ))[[3]]
@@ -79,10 +92,16 @@ for (i in 1:niter) {
 if (!file.exists("results")) {
   dir.create("results")
 }
-save(m_vec, time_df, prob_df, file = "results/Vecc_bias_lowdim.RData")
+save(m_vec, time_df, prob_df, file = paste0(
+  "results/Vecc_bias_lowdim_exp",
+  prob_ind, ".RData"
+))
 
 # Plotting -----------------------------------
-load("results/Vecc_bias_lowdim.RData")
+load(paste0(
+  "results/Vecc_bias_lowdim_exp",
+  prob_ind, ".RData"
+))
 library(ggplot2)
 library(tidyr)
 box_plt_low_dim <- function(mydf, yLim = NULL, yName = NULL,
@@ -95,10 +114,27 @@ box_plt_low_dim <- function(mydf, yLim = NULL, yName = NULL,
     scale_y_continuous(limits = yLim, name = yName, trans = yTrans) +
     geom_boxplot()
 }
+box_plt_low_dim_Botev_only <- function(mydf, yLim = NULL, yName = NULL,
+                                       yTrans = "identity") {
+  mtd_names <- c(paste0("m = ", m_vec), "ET")
+  colnames(mydf) <- mtd_names
+  mydf_pivot <- pivot_longer(mydf, cols = 1:ncol(mydf), names_to = "method")
+  ggplot(mydf_pivot, aes(x = method, y = value)) +
+    scale_x_discrete(limits = mtd_names, name = NULL) +
+    scale_y_continuous(limits = yLim, name = yName, trans = yTrans) +
+    geom_boxplot()
+}
 if (!file.exists("plots")) {
   dir.create("plots")
 }
-box_plt_low_dim(prob_df, yName = "MVN prob", yLim = c(0, 1e-7))
-ggsave("plots/prob_lowdim_exp1.pdf", width = 5, height = 5)
+box_plt_low_dim(prob_df, yName = "log MVN prob", yTrans = "log2")
+ggsave(paste0("plots/logprob_lowdim_exp", prob_ind, ".pdf"),
+  width = 5,
+  height = 5
+)
+box_plt_low_dim_Botev_only(prob_df[, 1:(length(m_vec) + 1)],
+  yName = "MVN prob"
+)
+ggsave(paste0("plots/prob_lowdim_exp", prob_ind, ".pdf"), width = 5, height = 5)
 box_plt_low_dim(time_df, yName = "time (seconds)", yTrans = "log2")
-ggsave("plots/time_lowdim_exp1.pdf", width = 5, height = 5)
+ggsave(paste0("plots/time_lowdim_exp", prob_ind, ".pdf"), width = 5, height = 5)
