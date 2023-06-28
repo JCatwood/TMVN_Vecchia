df21dbf6e74348a821e9e754a1030c41f530b23f
Jian Cao <jian.cao@kaust.edu.sa>
2023-06-28 11:40:31 -0500

    Included two more orderings for comparison

--- experiments/ordering_bias.R
+++ experiments/ordering_bias.R
@@ -76,8 +76,8 @@ prob_obj_reorder <- get(paste0("prob", prob_ind, "_gen"))(n, d, retDenseCov = T,
   reorder = T)
 ## Iteratively compute the same MVN prob -----------------------
 niter <- 30
-time_df <- data.frame(matrix(NA, niter, 4))
-prob_df <- data.frame(matrix(NA, niter, 4))
+time_df <- data.frame(matrix(NA, niter, 6))
+prob_df <- data.frame(matrix(NA, niter, 6))
 for (i in 1:niter) {
   ### Compute MVN prob with idea V -----------------------
   time_Vecc <- system.time(est_Vecc <- VeccTMVN::pmvn(prob_obj$a, prob_obj$b, 0,
@@ -86,8 +86,22 @@ for (i in 1:niter) {
     m = m, verbose = T,
     NLevel1 = 10, NLevel2 = 1e3
   ))[[3]]
-  time_Vecc_reorder <- system.time(
-    est_Vecc_reorder <- VeccTMVN::pmvn(
+  time_Vecc_FIC_reorder <- system.time(est_Vecc_FIC_reorder <-
+    VeccTMVN::pmvn(prob_obj$a, prob_obj$b, 0,
+      locs = prob_obj$locs, covName = prob_obj$cov_name,
+      reorder = 1, covParms = prob_obj$cov_parms,
+      m = m, verbose = T,
+      NLevel1 = 10, NLevel2 = 1e3
+    ))[[3]]
+  time_Vecc_Vecc_reorder <- system.time(est_Vecc_Vecc_reorder <-
+    VeccTMVN::pmvn(prob_obj$a, prob_obj$b, 0,
+      locs = prob_obj$locs, covName = prob_obj$cov_name,
+      reorder = 2, covParms = prob_obj$cov_parms,
+      m = m, verbose = T,
+      NLevel1 = 10, NLevel2 = 1e3
+    ))[[3]]
+  time_Vecc_univar_reorder <- system.time(
+    est_Vecc_univar_reorder <- VeccTMVN::pmvn(
       prob_obj_reorder$a, prob_obj_reorder$b, 0,
       locs = prob_obj_reorder$locs, covName = prob_obj_reorder$cov_name,
       reorder = 0, covParms = prob_obj_reorder$cov_parms,
@@ -105,8 +119,14 @@ for (i in 1:niter) {
     lb = prob_obj_reorder$a, ub = prob_obj_reorder$b, B = 1e4
   ))[[3]]
   ### save results ------------------------
-  time_df[i, ] <- c(time_Vecc, time_Vecc_reorder, time_TN, time_TN_reorder)
-  prob_df[i, ] <- c(est_Vecc, est_Vecc_reorder, est_TN, est_TN_reorder)
+  time_df[i, ] <- c(
+    time_Vecc, time_Vecc_FIC_reorder, time_Vecc_Vecc_reorder,
+    time_Vecc_univar_reorder, time_TN, time_TN_reorder
+  )
+  prob_df[i, ] <- c(
+    est_Vecc, est_Vecc_FIC_reorder, est_Vecc_Vecc_reorder,
+    est_Vecc_univar_reorder, est_TN, est_TN_reorder
+  )
 }
 if (!file.exists("results")) {
   dir.create("results")
@@ -125,7 +145,7 @@ library(ggplot2)
 library(tidyr)
 box_plt_low_dim <- function(mydf, yLim = NULL, yName = NULL,
                             yTrans = "identity") {
-  mtd_names <- c("Vecc", "Vecc_reorder", "ET", "ET_reorder")
+  mtd_names <- c("VSOV", "VSOV_FIC", "VSOV_Vecc", "VSOV_uni", "MET", "MET_uni")
   colnames(mydf) <- mtd_names
   mydf_pivot <- pivot_longer(mydf, cols = 1:ncol(mydf), names_to = "method")
   ggplot(mydf_pivot, aes(x = method, y = value)) +
