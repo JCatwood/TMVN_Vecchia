7e18ef9b9b86c811ec89c845eb917787d049479a
JCatwood <jcao2416@gmailc.om>
2024-09-06 15:37:24 -0500

    Changed PCE_modeling by splitting data into training and testing. Working on computing a score for the testing data in TX

--- experiments/PCE_modeling.R
+++ experiments/PCE_modeling.R
@@ -62,6 +62,21 @@ locs_scaled[, d] <- (locs_scaled[, d] - min(locs_scaled[, d])) /
 #     ggplot(aes(x = lon, y = lat, color=log_PCE)) +
 #     geom_point()
 # }
+# data splitting ---------------------------------------
+mask_censor <- rep(FALSE, n)
+mask_censor[ind_censor] <- TRUE
+n_train <- round(n * 0.8)
+set.seed(123)
+ind_train <- sample(1:n, n_train, replace = FALSE)
+y_train <- y[ind_train]
+y_scaled_train <- y_scaled[ind_train]
+y_scaled_test <- y_scaled[-ind_train]
+b_scaled_train <- b_scaled[ind_train]
+b_scaled_test <- b_scaled[-ind_train]
+locs_scaled_train <- locs_scaled[ind_train, ]
+locs_scaled_test <- locs_scaled[-ind_train, ]
+locs_train <- locs[ind_train, ]
+locs_test <- locs[-ind_train, ]
 # model fitting -------------------------------
 cov_name <- "matern_spacetime"
 covparms_init <- c(1, 0.1, 0.1, 0.5) # var, range1, range2, nugget
@@ -105,38 +120,45 @@ lonlat_to_state <- function(locs) {
   ii <- as.integer(st_intersects(pts, states))
   state_names[ii]
 }
-state_names <- lonlat_to_state(data.frame(locs))
-ind_Texas <- which(state_names == "Texas")
+state_names_train <- lonlat_to_state(data.frame(locs_train))
+ind_Texas_train <- which(state_names_train == "Texas")
 # Define a enveloping box for Texas -------------------------------------------
 ## TX boundary lat: 25.83333 to 36.5 lon: -93.51667 to -106.6333
-ind_Texas_box <- which((locs[, 1] < -93.02) & (locs[, 1] > -107.13) &
-  (locs[, 2] > 25.33) & (locs[, 2] < 37))
-# Sample at locations given by `ind_Texas_big` --------------------------------
+ind_Texas_box_train <- which((locs_train[, 1] < -93.02) & (locs_train[, 1] > -107.13) &
+  (locs_train[, 2] > 25.33) & (locs_train[, 2] < 37))
+# Sample at locations given by `ind_Texas_big_train` --------------------------------
 load("results/PCE_modeling.RData")
-ind_Texas_big <- ind_Texas
-ind_obs <- which(!is.na(y))
-ind_Texas_big <- union(ind_Texas_big, ind_obs)
-ind_censor_Texas_big <- which(is.na(y[ind_Texas_big]))
-locs_scaled_Texas_big <- locs_scaled[ind_Texas_big, , drop = F]
-y_scaled_Texas_big <- y_scaled[ind_Texas_big]
-b_scaled_Texas_big <- b_scaled[ind_Texas_big]
+ind_Texas_big_train <- ind_Texas_train
+ind_obs_train <- which(!is.na(y_train))
+ind_Texas_big_train <- union(ind_Texas_big_train, ind_obs_train)
+ind_censor_Texas_big_train <- which(is.na(y_train[ind_Texas_big_train]))
+locs_scaled_Texas_big_train <- locs_scaled_train[ind_Texas_big_train, , drop = F]
+y_scaled_Texas_big_train <- y_scaled_train[ind_Texas_big_train]
+b_scaled_Texas_big_train <- b_scaled_train[ind_Texas_big_train]
 N <- 1000
 covparms <- c(opt_obj$par[1:3], smoothness, opt_obj$par[4])
-# time_sim_Texas_big <- system.time(
-#   samp_Texas_big <- ptmvrandn(
-#     locs_scaled_Texas_big,
-#     ind_censor_Texas_big,
-#     y_scaled_Texas_big,
-#     b_scaled_Texas_big, cov_name, covparms,
-#     m = 50, N = N
+# time_sim_Texas_big_train <- system.time(
+#   samp_Texas_big_train <- ptmvrandn(
+#     locs_scaled_Texas_big_train,
+#     ind_censor_Texas_big_train,
+#     y_scaled_Texas_big_train,
+#     b_scaled_Texas_big_train, cov_name, covparms,
+#     m = 50, N = N, reorder = FALSE
 #   )
 # )[[3]]
 # if (!file.exists("results")) {
 #   dir.create("results")
 # }
-# save(samp_Texas_big, time_sim_Texas_big, covparms, cov_name, ind_Texas_big,
+# save(samp_Texas_big_train, time_sim_Texas_big_train, covparms, 
+#      cov_name, ind_Texas_big_train,
 #   file = "results/PCE_modeling_sample.RData"
 # )
+# MSE at testing ----------------------------------
+state_names_test <- lonlat_to_state(data.frame(locs_test))
+ind_Texas_test <- which(state_names_test == "Texas")
+locs_scaled_Texas_test <- locs_scaled_test[ind_Texas_test, ]
+y_scaled_Texas_test <- y_scaled_test[ind_Texas_test]
+b_scaled_Texas_test <- b_scaled_test[ind_Texas_test]
 # plots -------------------------------------------
 library(fields)
 library(RColorBrewer)
@@ -156,43 +178,30 @@ TX_grid_scaled <- cbind(TX_grid_scaled, rep(1, nrow(TX_grid)))
 colnames(TX_grid_scaled) <- c("lon", "lat", "time")
 ## pred_VMET --------------------------------------
 cov_mat <- get(cov_name)(covparms, rbind(
-  locs_scaled_Texas_big,
+  locs_scaled_Texas_big_train,
   as.matrix(TX_grid_scaled)
 ))
-n_obs <- nrow(locs_scaled_Texas_big)
+n_obs <- nrow(locs_scaled_Texas_big_train)
 n_grid <- nrow(TX_grid)
 pred_VMET <- rowMeans(cov_mat[(n_obs + 1):(n_obs + n_grid), 1:n_obs] %*%
-  solve(cov_mat[1:n_obs, 1:n_obs], samp_Texas_big))
+  solve(cov_mat[1:n_obs, 1:n_obs], samp_Texas_big_train))
 pred_VMET <- pred_VMET * sd(y, na.rm = T) + mean(y, na.rm = T)
 pred_VMET[!(lonlat_to_state(TX_grid) == "Texas") |
   (is.na(lonlat_to_state(TX_grid)))] <- NA
 ## pred_LOD_GP --------------------------------------
-y_aug <- samp_Texas_big[, 1]
-y_aug[ind_censor_Texas_big] <- b_scaled_Texas_big[ind_censor_Texas_big]
+y_aug <- samp_Texas_big_train[, 1]
+y_aug[ind_censor_Texas_big_train] <- 
+  b_scaled_Texas_big_train[ind_censor_Texas_big_train]
 cov_mat <- get(cov_name)(covparms, rbind(
-  locs_scaled_Texas_big,
+  locs_scaled_Texas_big_train,
   as.matrix(TX_grid_scaled)
 ))
-n_obs <- nrow(locs_scaled_Texas_big)
+n_obs <- nrow(locs_scaled_Texas_big_train)
 n_grid <- nrow(TX_grid)
 pred_GP_aug <- cov_mat[(n_obs + 1):(n_obs + n_grid), 1:n_obs] %*%
   solve(cov_mat[1:n_obs, 1:n_obs], y_aug)
 pred_GP_aug <- pred_GP_aug * sd(y, na.rm = T) + mean(y, na.rm = T)
 pred_GP_aug[!(lonlat_to_state(TX_grid) == "Texas") |
-  (is.na(lonlat_to_state(TX_grid)))] <- NA
-## pred_GP --------------------------------------
-locs_obs_scaled <- locs_scaled[-ind_censor, , drop = F]
-y_obs_scaled <- y_scaled[-ind_censor]
-cov_mat <- get(cov_name)(covparms, rbind(
-  locs_obs_scaled,
-  as.matrix(TX_grid_scaled)
-))
-n_obs <- nrow(locs_obs_scaled)
-n_grid <- nrow(TX_grid)
-pred_GP <- rowMeans(cov_mat[(n_obs + 1):(n_obs + n_grid), 1:n_obs] %*%
-  solve(cov_mat[1:n_obs, 1:n_obs], y_obs_scaled))
-pred_GP <- pred_GP * sd(y, na.rm = T) + mean(y, na.rm = T)
-pred_GP[!(lonlat_to_state(TX_grid) == "Texas") |
   (is.na(lonlat_to_state(TX_grid)))] <- NA
 ## actual plot ----------------------------------------
 pdf(file = "plots/PCE_modeling.pdf", width = 12, height = 5)
@@ -204,13 +213,13 @@ image.plot(lon_grid, lat_grid, matrix(pred_VMET, 100, 100),
   mgp = c(2, 1, 0), zlim = c(-13.1, 0.07)
 )
 points(
-  locs[ind_Texas_big[ind_censor_Texas_big], 1],
-  locs[ind_Texas_big[ind_censor_Texas_big], 2],
+  locs_train[ind_Texas_big_train[ind_censor_Texas_big_train], 1],
+  locs[ind_Texas_big_train[ind_censor_Texas_big_train], 2],
   col = "grey",
   cex = 0.6, pch = 1,
 )
 points(
-  x = locs[ind_obs, 1], y = locs[ind_obs, 2], col = "black",
+  x = locs_train[ind_obs_train, 1], y = locs_train[ind_obs_train, 2], col = "black",
   cex = 0.6, pch = 4,
 )
 # fields::US(xlim = c(-106.6, -93.5), ylim = c(25.8, 36.6), add = T)
@@ -221,13 +230,13 @@ image.plot(lon_grid, lat_grid, matrix(pred_GP_aug, 100, 100),
   mgp = c(2, 1, 0), zlim = c(-13.1, 0.07)
 )
 points(
-  locs[ind_Texas_big[ind_censor_Texas_big], 1],
-  locs[ind_Texas_big[ind_censor_Texas_big], 2],
+  locs_train[ind_Texas_big_train[ind_censor_Texas_big_train], 1],
+  locs[ind_Texas_big_train[ind_censor_Texas_big_train], 2],
   col = "grey",
   cex = 0.6, pch = 1,
 )
 points(
-  x = locs[ind_obs, 1], y = locs[ind_obs, 2], col = "black",
+  x = locs_train[ind_obs_train, 1], y = locs_train[ind_obs_train, 2], col = "black",
   cex = 0.6, pch = 4,
 )
 dev.off()
