55b6f6e32194e1c2b1737bb91153891e2b281e42
Jian Cao <jcao2416@gmail.com>
2024-09-19 12:37:53 -0500

    Working on highdim experiments. Weird err occurred. Going to implement univar ordering because other's implementation aren't repliable enough

--- experiments/Vecc_bias_highdim.R
+++ experiments/Vecc_bias_highdim.R
@@ -48,7 +48,7 @@ set.seed(321)
 n <- 6400
 d <- 2
 m_vec <- seq(from = 30, to = 50, by = 10)
-m_ord <- 30
+N_SOV_TLR <- c(1e4, 2e4, 4e4)
 prob_ind <- 1
 prob_obj <- get(paste0("prob", prob_ind, "_gen"))(n, d, retDenseCov = T)
 a <- prob_obj$a
@@ -60,51 +60,65 @@ cov_parms <- prob_obj$cov_parms
 z_order <- tlrmvnmvt::zorder(locs)
 ## Iteratively compute the same MVN prob -----------------------
 niter <- 30
-time_df <- data.frame(matrix(NA, niter, 2 + length(m_vec)))
-prob_df <- data.frame(matrix(NA, niter, 2 + length(m_vec)))
-# for (i in 1:niter) {
-#   est_Vecc <- rep(NA, length(m_vec))
-#   time_Vecc <- rep(NA, length(m_vec))
-#   for (j in 1:length(m_vec)) {
-#     ### Compute MVN prob with idea V -----------------------
-#     m <- m_vec[j]
-#     time_Vecc[j] <- system.time(est_Vecc[j] <- VeccTMVN::pmvn(a, b, 0,
-#       locs = locs, covName = cov_name,
-#       reorder = 2, covParms = cov_parms,
-#       m = m, verbose = T,
-#       NLevel1 = 10, NLevel2 = 1e4, m_ord = m # m_ord
-#     ))[[3]]
-#   }
-#   ### Compute MVN prob with other methods -----------------------
-#   err_obj <- try(
-#     time_TLR <- system.time(
-#       est_TLR <- tlrmvnmvt::pmvn(a[z_order], b[z_order],
-#         sigma = cov_mat[z_order, z_order],
-#         algorithm = tlrmvnmvt::TLRQMC(N = 5000, m = sqrt(n), epsl = 1e-6)
-#       )
-#     )[[3]]
-#   )
-#   if (class(err_obj) == "try-error") {
-#     time_TLR <- NA
-#     est_TLR <- NA
-#   }
-#   time_SOV <- system.time(
-#     est_SOV <- tlrmvnmvt::pmvn(a, b,
-#       sigma = cov_mat,
-#       algorithm = tlrmvnmvt::GenzBretz(N = 5000)
-#     )
-#   )[[3]]
-#   ### save results ------------------------
-#   time_df[i, ] <- c(time_Vecc, time_TLR, time_SOV)
-#   prob_df[i, ] <- c(est_Vecc, est_TLR, est_SOV)
-# }
-# if (!file.exists("results")) {
-#   dir.create("results")
-# }
-# save(m_vec, time_df, prob_df, file = paste0(
-#   "results/Vecc_bias_highdim_exp",
-#   prob_ind, ".RData"
-# ))
+time_df <- data.frame(matrix(NA, niter, 2 * length(N_SOV_TLR) + length(m_vec)))
+prob_df <- data.frame(matrix(NA, niter, 2 * length(N_SOV_TLR) + length(m_vec)))
+for (i in 1:niter) {
+  est_Vecc <- rep(NA, length(m_vec))
+  time_Vecc <- rep(NA, length(m_vec))
+  for (j in 1:length(m_vec)) {
+    cat("VeccTMVN", j, "\n")
+    ### Compute MVN prob with idea V -----------------------
+    m <- m_vec[j]
+    time_Vecc[j] <- system.time(est_Vecc[j] <- VeccTMVN::pmvn(a, b, 0,
+      locs = locs, covName = cov_name,
+      reorder = 3, covParms = cov_parms,
+      m = m, verbose = T,
+      NLevel1 = 10, NLevel2 = 1e4
+    ))[[3]]
+  }
+  ### Compute MVN prob with other methods -----------------------
+  est_TLR <- rep(NA, length(N_SOV_TLR))
+  time_TLR <- rep(NA, length(N_SOV_TLR))
+  for (j in 1:length(N_SOV_TLR)) {
+    cat("TLR", j, "\n")
+    N <- N_SOV_TLR[j]
+    err_obj <- try(
+      time_TLR[j] <- system.time(
+        est_TLR[j] <- tlrmvnmvt::pmvn(a[z_order], b[z_order],
+          sigma = cov_mat[z_order, z_order],
+          algorithm = tlrmvnmvt::TLRQMC(N = round(N / 20), m = sqrt(n), epsl = 1e-6)
+        )
+      )[[3]]
+    )
+    if (class(err_obj) == "try-error") {
+      time_TLR[j] <- NA
+      est_TLR[j] <- NA
+    }
+  }
+
+  est_SOV <- rep(NA, length(N_SOV_TLR))
+  time_SOV <- rep(NA, length(N_SOV_TLR))
+  for (j in 1:length(N_SOV_TLR)) {
+    cat("SOV", j, "\n")
+    N <- N_SOV_TLR[j]
+    time_SOV[j] <- system.time(
+      est_SOV[j] <- tlrmvnmvt::pmvn(a, b,
+        sigma = cov_mat,
+        algorithm = tlrmvnmvt::GenzBretz(N = round(N / 20))
+      )
+    )[[3]]
+  }
+  ### save results ------------------------
+  time_df[i, ] <- c(time_Vecc, time_TLR, time_SOV)
+  prob_df[i, ] <- c(est_Vecc, est_TLR, est_SOV)
+}
+if (!file.exists("results")) {
+  dir.create("results")
+}
+save(m_vec, N_SOV_TLR, time_df, prob_df, file = paste0(
+  "results/Vecc_bias_highdim_exp",
+  prob_ind, ".RData"
+))
 
 # Plotting -----------------------------------
 load(paste0(
