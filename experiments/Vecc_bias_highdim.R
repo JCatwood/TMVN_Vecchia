0a7443d7b6d29fe495c51ccfc3ef547e5320a7ec
JCatwood <jcao2416@gmailc.om>
2024-09-27 13:23:46 -0500

    Adjusted the plotting design

--- experiments/Vecc_bias_highdim.R
+++ experiments/Vecc_bias_highdim.R
@@ -52,7 +52,7 @@ m_vec <- c(30, 50, 70)
 N_SOV <- c(2e4, 5e4, 10e4)
 N_TLR <- c(2e4, 5e4, 10e4)
 if (length(myargs) > 0) {
-  prob_ind <- as.numeric(myargs[1])  
+  prob_ind <- as.numeric(myargs[1])
 } else {
   prob_ind <- 1
 }
@@ -66,66 +66,66 @@ cov_parms <- prob_obj$cov_parms
 z_order <- tlrmvnmvt::zorder(locs)
 ## Iteratively compute the same MVN prob -----------------------
 niter <- 30
-time_df <- data.frame(matrix(NA, niter, length(N_TLR) + length(N_SOV) + length(m_vec)))
-prob_df <- data.frame(matrix(NA, niter, length(N_TLR) + length(N_SOV) + length(m_vec)))
-for (i in 1:niter) {
-  est_Vecc <- rep(NA, length(m_vec))
-  time_Vecc <- rep(NA, length(m_vec))
-  for (j in 1:length(m_vec)) {
-    cat("VeccTMVN", j, "\n")
-    ### Compute MVN prob with idea V -----------------------
-    m <- m_vec[j]
-    time_Vecc[j] <- system.time(est_Vecc[j] <- VeccTMVN::pmvn(
-      a, b, 0,
-      locs = locs, covName = cov_name,
-      reorder = 3, covParms = cov_parms,
-      m = m, verbose = T,
-      NLevel1 = 10, NLevel2 = 1e4
-    ))[[3]]
-  }
-  ### Compute MVN prob with other methods -----------------------
-  est_TLR <- rep(NA, length(N_TLR))
-  time_TLR <- rep(NA, length(N_TLR))
-  for (j in 1:length(N_TLR)) {
-    cat("TLR", j, "\n")
-    N <- N_TLR[j]
-    err_obj <- try(
-      time_TLR[j] <- system.time(
-        est_TLR[j] <- tlrmvnmvt::pmvn(a[z_order], b[z_order],
-          sigma = cov_mat[z_order, z_order],
-          algorithm = tlrmvnmvt::TLRQMC(N = round(N / 20), m = sqrt(n), epsl = 1e-6)
-        )
-      )[[3]]
-    )
-    if (class(err_obj) == "try-error") {
-      time_TLR[j] <- NA
-      est_TLR[j] <- NA
-    }
-  }
-
-  est_SOV <- rep(NA, length(N_SOV))
-  time_SOV <- rep(NA, length(N_SOV))
-  for (j in 1:length(N_SOV)) {
-    cat("SOV", j, "\n")
-    N <- N_SOV[j]
-    time_SOV[j] <- system.time(
-      est_SOV[j] <- tlrmvnmvt::pmvn(a, b,
-        sigma = cov_mat,
-        algorithm = tlrmvnmvt::GenzBretz(N = round(N / 20))
-      )
-    )[[3]]
-  }
-  ### save results ------------------------
-  time_df[i, ] <- c(time_Vecc, time_TLR, time_SOV)
-  prob_df[i, ] <- c(est_Vecc, est_TLR, est_SOV)
-}
-if (!file.exists("results")) {
-  dir.create("results")
-}
-save(m_vec, N_SOV, N_TLR, time_df, prob_df, file = paste0(
-  "results/Vecc_bias_highdim_exp",
-  prob_ind, ".RData"
-))
+# time_df <- data.frame(matrix(NA, niter, length(N_TLR) + length(N_SOV) + length(m_vec)))
+# prob_df <- data.frame(matrix(NA, niter, length(N_TLR) + length(N_SOV) + length(m_vec)))
+# for (i in 1:niter) {
+#   est_Vecc <- rep(NA, length(m_vec))
+#   time_Vecc <- rep(NA, length(m_vec))
+#   for (j in 1:length(m_vec)) {
+#     cat("VeccTMVN", j, "\n")
+#     ### Compute MVN prob with idea V -----------------------
+#     m <- m_vec[j]
+#     time_Vecc[j] <- system.time(est_Vecc[j] <- VeccTMVN::pmvn(
+#       a, b, 0,
+#       locs = locs, covName = cov_name,
+#       reorder = 3, covParms = cov_parms,
+#       m = m, verbose = T,
+#       NLevel1 = 10, NLevel2 = 1e4
+#     ))[[3]]
+#   }
+#   ### Compute MVN prob with other methods -----------------------
+#   est_TLR <- rep(NA, length(N_TLR))
+#   time_TLR <- rep(NA, length(N_TLR))
+#   for (j in 1:length(N_TLR)) {
+#     cat("TLR", j, "\n")
+#     N <- N_TLR[j]
+#     err_obj <- try(
+#       time_TLR[j] <- system.time(
+#         est_TLR[j] <- tlrmvnmvt::pmvn(a[z_order], b[z_order],
+#           sigma = cov_mat[z_order, z_order],
+#           algorithm = tlrmvnmvt::TLRQMC(N = round(N / 20), m = sqrt(n), epsl = 1e-6)
+#         )
+#       )[[3]]
+#     )
+#     if (class(err_obj) == "try-error") {
+#       time_TLR[j] <- NA
+#       est_TLR[j] <- NA
+#     }
+#   }
+#
+#   est_SOV <- rep(NA, length(N_SOV))
+#   time_SOV <- rep(NA, length(N_SOV))
+#   for (j in 1:length(N_SOV)) {
+#     cat("SOV", j, "\n")
+#     N <- N_SOV[j]
+#     time_SOV[j] <- system.time(
+#       est_SOV[j] <- tlrmvnmvt::pmvn(a, b,
+#         sigma = cov_mat,
+#         algorithm = tlrmvnmvt::GenzBretz(N = round(N / 20))
+#       )
+#     )[[3]]
+#   }
+#   ### save results ------------------------
+#   time_df[i, ] <- c(time_Vecc, time_TLR, time_SOV)
+#   prob_df[i, ] <- c(est_Vecc, est_TLR, est_SOV)
+# }
+# if (!file.exists("results")) {
+#   dir.create("results")
+# }
+# save(m_vec, N_SOV, N_TLR, time_df, prob_df, file = paste0(
+#   "results/Vecc_bias_highdim_exp",
+#   prob_ind, ".RData"
+# ))
 
 # Plotting -----------------------------------
 load(paste0(
@@ -135,6 +135,7 @@ load(paste0(
 library(ggplot2)
 library(tidyr)
 library(scales)
+library(dplyr)
 time_vs_err_plt <- function(probDf, timeDf) {
   col_names <- c(
     paste("VMET", m_vec, sep = "_"),
@@ -143,50 +144,64 @@ time_vs_err_plt <- function(probDf, timeDf) {
   )
   colnames(probDf) <- col_names
   colnames(timeDf) <- col_names
-  
-  if(prob_ind == 3) {
-    colname_benchmark <- paste("SOV", max(N_SOV), sep = "_")
-  } else {
-    colname_benchmark <- paste("VMET", max(m_vec), sep = "_")
-  }
-  
-  benchmark <- mean(probDf[[colname_benchmark]])  
-  rmse <- sqrt(colMeans((probDf - benchmark)^2))
-  secs <- colMeans(timeDf)
-  mydf <- data.frame(
-    time = secs, rmse = rmse,
-    method = gsub("_.*", "", col_names)
-  )
-  # log2 trans to x and y ticks are applied by default
-  breaks <- exp(seq(
-    from = log(min(mydf$rmse) / 2),
-    to = log(max(mydf$rmse)), length.out = 5
-  ))
-  
-  # test
-  probDf_pivot <- pivot_longer(probDf, cols = 1 : ncol(probDf),
-                               names_to = "method")
+  colname_benchmark <- paste("SOV", max(N_SOV), sep = "_")
+  benchmark <- mean(probDf[[colname_benchmark]])
+
+  probDf_pivot <- pivot_longer(probDf,
+    cols = 1:ncol(probDf),
+    names_to = "method"
+  ) %>% mutate(grp = gsub("_.*", "", method))
+  probDf_pivot$method <- factor(probDf_pivot$method, levels = col_names)
+  probDf_pivot$grp <- factor(probDf_pivot$grp, levels = c("VMET", "TLR", "SOV"))
+  timeDf_new <- data.frame(method = col_names, time = colMeans(timeDf)) %>%
+    mutate(grp = gsub("_.*", "", col_names))
+  coeff <- (log(max(probDf_pivot$value)) - log(min(probDf_pivot$value))) /
+    (max(timeDf_new$time) - min(timeDf_new$time))
+  min_log_val <- log(min(probDf_pivot$value))
+  time_trans <- exp((timeDf_new$time - min(timeDf_new$time)) * coeff +
+    log(min(probDf_pivot$value)))
+  timeDf_new <- timeDf_new %>% mutate(time_trans = time_trans)
+  timeDf_new$method <- factor(timeDf_new$method, levels = col_names)
+  timeDf_new$grp <- factor(timeDf_new$grp, levels = c("VMET", "TLR", "SOV"))
+
   ggplot(data = probDf_pivot, mapping = aes(x = method, y = value)) +
-  geom_boxplot() +
-  scale_y_continuous(
-    name = "Estimates", trans = "log2"
-  )
-  
-  ggplot(data = mydf, mapping = aes(x = time, y = rmse)) +
-    geom_point(mapping = aes(colour = method, shape = method), size = 3) +
-    geom_line(mapping = aes(group = method, colour = method)) +
+    geom_boxplot(mapping = aes(fill = grp), alpha = 0.5) +
+    geom_line(
+      data = timeDf_new,
+      mapping = aes(x = method, y = time_trans, group = grp, color = grp)
+    ) +
+    geom_point(
+      data = timeDf_new,
+      mapping = aes(x = method, y = time_trans, group = grp, color = grp)
+    ) +
+    geom_hline(yintercept = benchmark, linetype = "dashed", color = "red") +
     scale_y_continuous(
-      name = "RMSE", trans = "log2", breaks = breaks,
-      labels = signif(breaks, digits = 2)
+      name = "probability estimates", trans = "log2",
+      breaks = signif(
+        2^(seq(
+          from = log2(min(probDf_pivot$value)),
+          to = log2(max(probDf_pivot$value)), length.out = 4
+        )),
+        digits = 2
+      ),
+      sec.axis = sec_axis(~ (log(.) - min_log_val) / coeff + min(timeDf_new$time),
+        name = "time (seconds)",
+        breaks = signif(
+          seq(
+            from = min(timeDf_new$time),
+            to = max(timeDf_new$time), length.out = 4
+          ),
+          digits = 1
+        )
+      )
     ) +
-    scale_x_continuous(
-      name = "time (seconds)", trans = "log2"
-    ) +
+    scale_x_discrete(labels = c(paste0("m", m_vec), paste0("N", c(N_TLR, N_SOV) / 10000))) +
     ggtitle(paste("Scenario", prob_ind)) +
     theme(
       text = element_text(size = 14),
       legend.title = element_blank(),
-      plot.title = element_text(hjust = 0.5)
+      legend.position = "bottom",
+      plot.title = element_text(hjust = 0.5), axis.title.x = element_blank()
     )
 }
 if (!file.exists("plots")) {
@@ -198,6 +213,3 @@ ggsave(
   width = 5,
   height = 5
 )
-
-
-
